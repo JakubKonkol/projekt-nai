27cf02bfbb45355c0f0538a882967707234976d2
JakubKonkol <s24406@pjwstk.edu.pl>
2024-01-23 13:34:18 +0100

    setup project

---
+++ summarizer.py
@@ -0,0 +1,11 @@
+from transformers import pipeline
+
+class Summarizer:
+    def __init__(self, model_name="facebook/bart-large-cnn", max_length=130, min_length=30, do_sample=False):
+        self.summarizer = pipeline("summarization", model=model_name)
+        self.max_length = max_length
+        self.min_length = min_length
+        self.do_sample = do_sample
+
+    def generate_summary(self, text):
+        return self.summarizer(text, max_length=self.max_length, min_length=self.min_length, do_sample=self.do_sample)
